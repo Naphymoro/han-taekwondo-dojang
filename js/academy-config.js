71fc7932ecbcd7d3f4a89422015f0de0e94d9306
Cecil Ouma <163507991+Naphymoro@users.noreply.github.com>
2026-05-30 00:47:51 +0300

    Use public Apps Script URL without workspace redirect

--- js/academy-config.js
+++ js/academy-config.js
@@ -1,1 +1,1 @@
-window.HAN_APPS_SCRIPT_URL = 'https://script.google.com/a/macros/aimsric.org/s/AKfycbzHMMyAYreiYG5t2ehafCMwnKoUqLxHajC7HmQIgLw4WahsyODwfGilw6GBpet2P8EjMw/exec';
+window.HAN_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzHMMyAYreiYG5t2ehafCMwnKoUqLxHajC7HmQIgLw4WahsyODwfGilw6GBpet2P8EjMw/exec';
