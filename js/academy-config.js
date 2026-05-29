f85d54c428a76bcad874a7fb218de4bfa1adc593
Cecil Ouma <163507991+Naphymoro@users.noreply.github.com>
2026-05-30 02:09:29 +0300

    Point frontend to latest Apps Script deployment

--- js/academy-config.js
+++ js/academy-config.js
@@ -1,1 +1,1 @@
-window.HAN_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzHMMyAYreiYG5t2ehafCMwnKoUqLxHajC7HmQIgLw4WahsyODwfGilw6GBpet2P8EjMw/exec';
+window.HAN_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbylbBBD7LKYWhaU1QJbhlK4FCW4gIgJt8rlaR1aU4Ff_7Tsf3UWiXf-va4Uo1vJO0euaQ/exec';
